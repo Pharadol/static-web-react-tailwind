cd47417c235edb6f586ed7233defb01a120dcea5
Pharadol <pharadol.n@gmail.com>
2024-03-27 15:39:00 +0700

    feat: clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import './App.scss'
 function App() {
-
   return (
     <>
-      <h1>Hello</h1>
-      <p className='text-green-500 text-4xl'>hello react</p>
+      <p className="text-green-500 text-4xl">hello react</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

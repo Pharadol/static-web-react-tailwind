c999d72c6c06ff8f3e229453625564f1bae62d1d
Pharadol <pharadol.n@gmail.com>
2024-03-28 17:10:08 +0700

    feat: static page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import FrontLayout from "./components/layouts/FrontLayout";
 import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Team from "./pages/team/Team";
+import Project from "./pages/project/Project";
 
 const AppRoutes = () => {
   return (
     <Routes>
-     <Route  element={<FrontLayout />}>
+      <Route element={<FrontLayout />}>
         <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/teams" element={<Team />} />
+        <Route path="/projects" element={<Project />} />
       </Route>
     </Routes>
   );
